7927dccad9356e19a9e55577be69901dcc6e3977
baltasar <baltaguirre@gmail.com>
2021-09-02 11:41:09 -0300

    commit

--- api/src/db.js
+++ api/src/db.js
@@ -10,7 +10,7 @@ let sequelize =
       database: DB_NAME,
       dialect: "postgres",
       host: DB_HOST,
-      port: process.env.PORT,
+      port: 5432,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
